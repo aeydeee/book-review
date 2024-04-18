8bf690587211c769607363d164e2da0bdb897d05
aeydeee <aeydeee@gmail.com>
2024-04-19 06:47:33 +0800

    added a new function for better query on database

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -21,19 +21,29 @@ class Book extends Model
         return $query->where('title', 'like', '%' . request('title') . '%');
     }
 
-    public function scopePopular(Builder $query, $from = null, $to = null): Builder
+    public function scopeWithReviewsCount(Builder $query, $from = null, $to = null): Builder
     {
         return $query->withCount([
             'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
-        ])
+        ]);
+    }
+
+    public function scopeWithAvgRating(Builder $query, $from = null, $to = null): Builder
+    {
+        return $query->withAvg([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating');
+    }
+
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder
+    {
+        return $query->withReviewsCount()
             ->orderBy('reviews_count', 'desc');
     }
 
     public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder
     {
-        return $query->withAvg([
-            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
-        ], 'rating')
+        return $query->withAvgRating()
             ->orderBy('reviews_avg_rating', 'desc');
     }
 
@@ -79,6 +89,16 @@ class Book extends Model
         return $query->highestRated(now()->subMonths(6), now())
             ->popular(now()->subMonths(6), now())
             ->minReviews(5);
+    }
+
+    protected static function booted(): void
+    {
+        static::updated(
+            static fn(Book $book) => cache()->forget('book:' . $book->book_id)
+        );
+        static::deleted(
+            static fn(Book $book) => cache()->forget('book:' . $book->book_id)
+        );
     }
 
 }
