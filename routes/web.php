2432f953f4166a8ccbd47fc55a767b4dcd41a7a9
aeydeee <aeydeee@gmail.com>
2024-04-21 07:55:47 +0800

    this is to use the rate limit for books.reviews.store when users creating a review

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,20 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/', static function () {
     return redirect(\route('books.index'));
 });
 
-Route::resource('books', BookController::class);
+Route::resource('books', BookController::class)
+    ->only(['index', 'show']);
+
+Route::resource('books.reviews', ReviewController::class)
+    ->scoped(['review' => 'book'])
+    ->only(['create']);
+
+Route::post('books/{book}/reviews', [ReviewController::class, 'store'])
+    ->middleware('throttle:reviews')
+    ->name('books.reviews.store');
